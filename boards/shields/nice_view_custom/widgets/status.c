056501cec58a97a802993325866db42f65e2ef46
Rico Scott <rico.r.scott.mil@army.mil>
2024-10-10 12:52:50 -0500

    fingers crossed2

--- boards/shields/nice_view_custom/widgets/status.c
+++ boards/shields/nice_view_custom/widgets/status.c
@@ -232,85 +232,85 @@ ZMK_SUBSCRIPTION(widget_battery_status, zmk_battery_state_changed);
 ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
 #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
 
-static void set_output_status(struct zmk_widget_status *widget,
-                              const struct output_status_state *state) {
-    widget->state.selected_endpoint = state->selected_endpoint;
-    widget->state.active_profile_index = state->active_profile_index;
-    widget->state.active_profile_connected = state->active_profile_connected;
-    widget->state.active_profile_bonded = state->active_profile_bonded;
+//static void set_output_status(struct zmk_widget_status *widget,
+//                              const struct output_status_state *state) {
+//    widget->state.selected_endpoint = state->selected_endpoint;
+//    widget->state.active_profile_index = state->active_profile_index;
+//    widget->state.active_profile_connected = state->active_profile_connected;
+//    widget->state.active_profile_bonded = state->active_profile_bonded;
+//
+//    draw_top(widget->obj, widget->cbuf, &widget->state);
+//    draw_middle(widget->obj, widget->cbuf2, &widget->state);
+//}
+//
+//static void output_status_update_cb(struct output_status_state state) {
+//    struct zmk_widget_status *widget;
+//    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_output_status(widget, &state); }
+//}
+//
+//static struct output_status_state output_status_get_state(const zmk_event_t *_eh) {
+//    return (struct output_status_state){
+//        .selected_endpoint = zmk_endpoints_selected(),
+//        .active_profile_index = zmk_ble_active_profile_index(),
+//        .active_profile_connected = zmk_ble_active_profile_is_connected(),
+//        .active_profile_bonded = !zmk_ble_active_profile_is_open(),
+//    };
+//}
+//
+//ZMK_DISPLAY_WIDGET_LISTENER(widget_output_status, struct output_status_state,
+//                            output_status_update_cb, output_status_get_state)
+//ZMK_SUBSCRIPTION(widget_output_status, zmk_endpoint_changed);
+//
+//#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+//ZMK_SUBSCRIPTION(widget_output_status, zmk_usb_conn_state_changed);
+//#endif
+//#if defined(CONFIG_ZMK_BLE)
+//ZMK_SUBSCRIPTION(widget_output_status, zmk_ble_active_profile_changed);
+//#endif
 
-    draw_top(widget->obj, widget->cbuf, &widget->state);
-    draw_middle(widget->obj, widget->cbuf2, &widget->state);
-}
-
-static void output_status_update_cb(struct output_status_state state) {
-    struct zmk_widget_status *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_output_status(widget, &state); }
-}
-
-static struct output_status_state output_status_get_state(const zmk_event_t *_eh) {
-    return (struct output_status_state){
-        .selected_endpoint = zmk_endpoints_selected(),
-        .active_profile_index = zmk_ble_active_profile_index(),
-        .active_profile_connected = zmk_ble_active_profile_is_connected(),
-        .active_profile_bonded = !zmk_ble_active_profile_is_open(),
-    };
-}
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_output_status, struct output_status_state,
-                            output_status_update_cb, output_status_get_state)
-ZMK_SUBSCRIPTION(widget_output_status, zmk_endpoint_changed);
-
-#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
-ZMK_SUBSCRIPTION(widget_output_status, zmk_usb_conn_state_changed);
-#endif
-#if defined(CONFIG_ZMK_BLE)
-ZMK_SUBSCRIPTION(widget_output_status, zmk_ble_active_profile_changed);
-#endif
-
-static void set_layer_status(struct zmk_widget_status *widget, struct layer_status_state state) {
-    widget->state.layer_index = state.index;
-    widget->state.layer_label = state.label;
-
-    draw_bottom(widget->obj, widget->cbuf3, &widget->state);
-}
-
-static void layer_status_update_cb(struct layer_status_state state) {
-    struct zmk_widget_status *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_layer_status(widget, state); }
-}
-
-static struct layer_status_state layer_status_get_state(const zmk_event_t *eh) {
-    uint8_t index = zmk_keymap_highest_layer_active();
-    return (struct layer_status_state){.index = index, .label = zmk_keymap_layer_name(index)};
-}
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, layer_status_update_cb,
-                            layer_status_get_state)
-
-ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
-
-static void set_wpm_status(struct zmk_widget_status *widget, struct wpm_status_state state) {
-    for (int i = 0; i < 9; i++) {
-        widget->state.wpm[i] = widget->state.wpm[i + 1];
-    }
-    widget->state.wpm[9] = state.wpm;
-
-    draw_top(widget->obj, widget->cbuf, &widget->state);
-}
-
-static void wpm_status_update_cb(struct wpm_status_state state) {
-    struct zmk_widget_status *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_wpm_status(widget, state); }
-}
-
-struct wpm_status_state wpm_status_get_state(const zmk_event_t *eh) {
-    return (struct wpm_status_state){.wpm = zmk_wpm_get_state()};
-};
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_wpm_status, struct wpm_status_state, wpm_status_update_cb,
-                            wpm_status_get_state)
-ZMK_SUBSCRIPTION(widget_wpm_status, zmk_wpm_state_changed);
+//static void set_layer_status(struct zmk_widget_status *widget, struct layer_status_state state) {
+//    widget->state.layer_index = state.index;
+//    widget->state.layer_label = state.label;
+//
+//    draw_bottom(widget->obj, widget->cbuf3, &widget->state);
+//}
+//
+//static void layer_status_update_cb(struct layer_status_state state) {
+//    struct zmk_widget_status *widget;
+//    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_layer_status(widget, state); }
+//}
+//
+//static struct layer_status_state layer_status_get_state(const zmk_event_t *eh) {
+//    uint8_t index = zmk_keymap_highest_layer_active();
+//    return (struct layer_status_state){.index = index, .label = zmk_keymap_layer_name(index)};
+//}
+//
+//ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, layer_status_update_cb,
+//                            layer_status_get_state)
+//
+//ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
+//
+//static void set_wpm_status(struct zmk_widget_status *widget, struct wpm_status_state state) {
+//    for (int i = 0; i < 9; i++) {
+//        widget->state.wpm[i] = widget->state.wpm[i + 1];
+//    }
+//    widget->state.wpm[9] = state.wpm;
+//
+//    draw_top(widget->obj, widget->cbuf, &widget->state);
+//}
+//
+//static void wpm_status_update_cb(struct wpm_status_state state) {
+//    struct zmk_widget_status *widget;
+//    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_wpm_status(widget, state); }
+//}
+//
+//struct wpm_status_state wpm_status_get_state(const zmk_event_t *eh) {
+//    return (struct wpm_status_state){.wpm = zmk_wpm_get_state()};
+//};
+//
+//ZMK_DISPLAY_WIDGET_LISTENER(widget_wpm_status, struct wpm_status_state, wpm_status_update_cb,
+//                            wpm_status_get_state)
+//ZMK_SUBSCRIPTION(widget_wpm_status, zmk_wpm_state_changed);
 
 int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     widget->obj = lv_obj_create(parent);
