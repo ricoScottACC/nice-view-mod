225c227182d969bdad17c3117bb7900d041217a6
Rico Scott <rico.r.scott.mil@army.mil>
2024-10-10 11:40:46 -0500

    test run

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -25,6 +25,25 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 LV_IMG_DECLARE(balloon);
 LV_IMG_DECLARE(mountain);
+LV_IMG_DECLARE(moon1);
+LV_IMG_DECLARE(moon2);
+LV_IMG_DECLARE(moon3);
+LV_IMG_DECLARE(moon4);
+LV_IMG_DECLARE(moon5);
+LV_IMG_DECLARE(moon6);
+LV_IMG_DECLARE(moon7);
+LV_IMG_DECLARE(moon8);
+
+const lv_img_dsc_t *anim_imgs[] = {
+    &moon1,
+    &moon2,
+    &moon3,
+    &moon4,
+    &moon5,
+    &moon6,
+    &moon7,
+    &moon8,
+};
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -114,9 +133,16 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
-    lv_obj_t *art = lv_img_create(widget->obj);
-    bool random = sys_rand32_get() & 1;
-    lv_img_set_src(art, random ? &balloon : &mountain);
+    //lv_obj_t *art = lv_img_create(widget->obj);
+    //bool random = sys_rand32_get() & 1;
+    //lv_img_set_src(art, random ? &balloon : &mountain);
+    //lv_img_set_src(art, &moon1);
+    lv_obj_t * art = lv_animimg_create(widget->obj);            //<--
+    lv_obj_center(art);                                         //<--
+    lv_animimg_set_src(art, (const void **) anim_imgs, 12);     //<--
+    lv_animimg_set_duration(art, 4000);                         //<--
+    lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);  //<--
+    lv_animimg_start(art);                                      //<--
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
